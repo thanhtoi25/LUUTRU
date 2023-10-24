0bb218457ee9f47c7200a05c53204dfadada85d0
thanhtoi25 <147480397+thanhtoi25@users.noreply.github.com>
2023-10-24 14:23:15 +0700

    Add files via upload

---
+++ baitap c.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int a[][100], int x, int y)  {
+    printf  ("nhap so dong");
+    scanf("%d", &x);
+    printf  ("nhap so cot");
+    scanf("%d", &y);
+    for( int i = 0 ; i < x ,  )
+
+
+}
