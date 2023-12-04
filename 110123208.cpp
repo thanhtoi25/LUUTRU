27bba8d475209a78fc9f0dbb393eef4589c20755
thanhtoi25 <147480397+thanhtoi25@users.noreply.github.com>
2023-12-04 10:40:11 +0700

    Add files via upload

---
+++ 110123208.cpp
@@ -0,0 +1,134 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+int output(int a[50][50], int m, int n)
+{
+    int i,j;
+    for(i=0;i<m;i++)
+	{
+        for(j=0;j<n;j++)
+		 {
+            printf("%d   ", a[i][j]);
+         }
+        printf("\n");
+    }
+}
+
+int sapxep(int m, int n, int a[50][50]){
+    int i, j;
+    for(i = 0;i<m;i++){
+        for(j = 0; j<n; j++){
+            int k,l;
+            for(k = i; k<m; k++){
+                int t = 0;
+                if(k == i) t = j+1;
+                for(l = t; l<n;l++){
+                    if(a[i][j] > a[k][l]){ 
+                        int temp = a[i][j];
+                        a[i][j] = a[k][l];
+                        a[k][l] = temp;
+                    }
+                }
+            }
+        }
+    }
+     
+     output(a,m,n);
+}
+
+int NHAP(int a[50][50], int m, int n)
+{
+
+	for(int i=0; i < m; i++){
+		for(int j=0; j<n ; j++){
+			printf("nhap phan tu A[%d][%d]", i+1,j+1);
+			scanf("%d",&a[i][j]);
+		}
+	} 
+
+
+}
+
+bool ktsnt(int p)
+{   if(p<2) {	return false; }
+
+    int i;
+    for(i=2;i<=sqrt((float)p);i++)
+	{
+		if(p%i == 0) 
+		return false;	
+	}
+	return true;
+ }
+int intong(int a[50][50], int m, int n)
+{
+    int i,j,t=0;
+    for(i=0;i<m;i++)
+	{
+        for(j=0;j<n;j++)
+		 {
+            if(a[i][j]%3 == 0){
+            	if(ktsnt(a[i][j]) == true){
+            		t = t + a[i][j];
+            	}
+            }
+         }
+    }  printf("tong cac so nguyen to chia het cho 3: %d\n",t);}
+
+
+
+int main()
+{
+int key,a[50][50],m,n;
+bool t = true;
+
+do{
+
+printf("****************************************************************\n");
+printf("*      1.NHAP MANG 2 CHIEU                                     *\n");
+printf("*      2.SAP XEP MANG THEO THU TU TANG DAN                     *\n");
+printf("*      3.TONG CAC PHAN TU LA SO NGUYEN TO CHIA HET CHO 3       *\n");
+printf("*      0.THOAT                                                 *\n");
+printf("****************************************************************\n");
+printf("\n AN PHIM CHON TUONG UNG: ");
+scanf("%d",&key);
+switch (key)
+
+{
+    case 1:
+    	{	
+		printf("nhap so hang: ");
+		scanf("%d",&m);
+		printf("nhap so cot: ");
+		scanf("%d",&n);
+    	NHAP(a,m,n);
+    	fflush(stdin);
+    	system("cls");
+      break;
+      }
+ 
+    case 2:{ 
+		
+  		sapxep(m,n,a);
+    	break;
+    	
+    }
+    	
+        
+    case 3:
+		intong(a, m, n);
+    	break;
+    case 0:
+    	t = false;
+    	break;
+    default:
+      printf("khong co phim nay");
+}
+
+	
+}while(t);
+
+
+}
